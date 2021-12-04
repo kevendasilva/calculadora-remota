8f99d21a97f0d26f0b1190f667b363640450e06e
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2021-12-04 02:09:53 -0300

    Refactor the Server of the remote calculator

--- lib/server/calculadora-server.py
+++ lib/server/calculadora-server.py
@@ -1,27 +1,39 @@
+# 
+# Definição dos comportamentos, do processo servidor - Calculadora remota
+# 
+
 from dotenv import load_dotenv
 import os
 import socket
 
 load_dotenv()
 
-i = 0
+# Informações do servidor
 host = os.getenv('HOST')
 port = int(os.getenv('PORT'))
+# Configurando o socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((host, port))
 
-while (True):
-    i += 1
-    print("Aguardando mensagem...")
+print("\n")
 
+while (True):
+    print("Aguardando nova mensagem...")
 
     try:
+        # Recebendo a mensagem
         operacao, address = sock.recvfrom(1024)
-        print("Mensagem recebida: ", operacao.decode("utf-8"))
+        
+        print("\n === Nova mensagem recebida ===")
+        print(f'Mensagem enviada do endereço: {address}')
+        print(f'Mensagem recebida: {operacao.decode("utf-8")}')
+        
+        # Determinando a resposta
         resposta = str(eval(operacao))
         print("Resposta: ", resposta)
 
         sock.sendto(bytes(resposta, 'utf-8'), address)
+        print(" === Resposta enviada === \n")
     except:
         print("Um erro foi identificado.")
         sock.sendto(bytes("Um erro foi identificado. Por favor, verifique a mensagem enviada e tente novamente",
